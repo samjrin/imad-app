9f3cddc69e01eb4a19305a6f64adfd20d1b2dcf0
samjrin <30766723+samjrin@users.noreply.github.com>
2017-08-08 21:23:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,16 +30,36 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
 
-    // Make a request to the server and sent the name
+    // Create a request object
+    var request = new XMLHttpRequest();
     
-    //Capture a list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status === 200) {
+//Capture a list of names and render it as a list
+    //var names = ['name1','name2','name3','name4'];
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for (var i=0; i<names.length; i++) {
         list +='<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+            }
+        }
+    };
+    
+    //make a request
+    //request.open('GET','http://samarthjoelram.imad.hasura-app.io/counter',true);
+    request.open('GET','http://samarthjoelram.imad.hasura-app.io/submit-name?name' + name, true);
+    request.send(null);
+
+    // Make a request to the server and sent the name
+    
+    
 };
     // render the variable in the correct span
     /*counter = counter + 1;
