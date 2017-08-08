13b5a7c2d6526b2326e6c8a97083ef4630b4544d
samjrin <30766723+samjrin@users.noreply.github.com>
2017-08-08 13:38:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,15 +82,16 @@ function createTemplate (data) {
     return htmltemplate;
 }
 
-var counter = 0;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
 app.get('/counter',function (req, rwes) {
     counter = counter +1;
     res.send(counter.toString())
-}
+});
 
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
