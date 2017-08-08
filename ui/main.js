34a9d7561db7bb35f6dc710fc3101b0f8c9385c1
samjrin <30766723+samjrin@users.noreply.github.com>
2017-08-08 21:36:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,8 +42,8 @@ submit.onclick = function () {
                  var names = request.responseText;
                  names = JSON.parse(names);
                  var list = '';
-                 for (var i=0; i<names.length; i++) {
-                     list +='<li>' + names[i] + '</li>';
+                 for (var i=0; i< names.length; i++) {
+                     list += '<li>' + names[i] + '</li>';
                  }
                  var ul = document.getElementById('namelist');
                  ul.innerHTML = list;
@@ -57,7 +57,7 @@ submit.onclick = function () {
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
 
-    request.open('GET','http://samarthjoelram.imad.hasura-app.io/submit-name?name' + name, true);
+    request.open('GET', 'http://samarthjoelram.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 
     // Make a request to the server and sent the name
