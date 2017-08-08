016cb14901f8f7bd8785733cddf46a5dad38b9a8
samjrin <30766723+samjrin@users.noreply.github.com>
2017-08-08 20:39:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name',function(req,res) {
+app.get('/submit-name/:name', function(req,res) {
     //get the name from the request
     var name = req.params.name; 
     
